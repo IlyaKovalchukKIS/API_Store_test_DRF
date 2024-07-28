bee4633bf83c4e5da8389a0d391af69856e50b88
user <mrlivedance@vk.com>
2024-07-28 22:23:49 +0300

    refactor parse site

--- store/management/commands/parse_site.py
+++ store/management/commands/parse_site.py
@@ -1,4 +1,5 @@
 from django.core.management import BaseCommand
+
 from config.settings import BASE_DIR
 from store.services import find_all_element, get_html, save_data, load_data_to_db
 
@@ -7,10 +8,11 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         url = "https://www.farpost.ru/vladivostok/service/construction/guard/+/Системы+видеонаблюдения/"
         try:
+            print("parse_site: Start")
             response = get_html(url)
             data = find_all_element(response, "tr", "bull-list-item-js -exact")
-            if not data and len(data) < 10:
-                raise Exception("Data not found")
-            save_data(BASE_DIR / "static/data.json", data)
+            if data and len(data.keys()) >= 10:
+                save_data(BASE_DIR / "static/data.json", data)
         finally:
             load_data_to_db()
+        print("parse_site: Done")
